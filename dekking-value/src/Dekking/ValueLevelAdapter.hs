29ad057664fed3b09e910a49038b62a919fb30f9
Tom Sydney Kerckhove <syd@cs-syd.eu>
2022-11-24 02:41:24 +0100

    no locking

--- dekking-value/src/Dekking/ValueLevelAdapter.hs
+++ dekking-value/src/Dekking/ValueLevelAdapter.hs
@@ -4,8 +4,6 @@
 -- values. Any dependency of this module cannot be code-covered.
 module Dekking.ValueLevelAdapter (coverageFileName, adaptValue) where
 
-import Control.Exception
-import GHC.IO.Handle.Lock (LockMode (ExclusiveLock), hLock, hUnlock)
 import System.IO
 import System.IO.Unsafe
 
@@ -14,10 +12,9 @@ coverageFileName = "coverage.dat"
 
 withCoverageHandle :: (Handle -> IO a) -> IO a
 withCoverageHandle func =
-  withFile coverageFileName AppendMode $ \h ->
-    bracket_ (hLock h ExclusiveLock) (hUnlock h) $ do
-      hSetBuffering h LineBuffering
-      func h
+  withFile coverageFileName AppendMode $ \h -> do
+    hSetBuffering h LineBuffering
+    func h
 
 {-# NOINLINE adaptValue #-}
 adaptValue :: String -> a -> a
