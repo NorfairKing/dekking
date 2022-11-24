2de3d157f5932c4eeadabcab9b8bcf7bf3d49eb1
Tom Sydney Kerckhove <syd@cs-syd.eu>
2022-11-24 17:35:29 +0100

    Use the two flags that are recommended about UnsafePerformIO

--- dekking-value/src/Dekking/ValueLevelAdapter.hs
+++ dekking-value/src/Dekking/ValueLevelAdapter.hs
@@ -1,3 +1,10 @@
+-- The docs here:
+-- https://hackage.haskell.org/package/base-4.17.0.0/docs/System-IO-Unsafe.html#v:unsafePerformIO
+-- recommend these two compiler flags:
+-- - Use the compiler flag -fno-cse to prevent common sub-expression elimination being performed on the module, which might combine two side effects that were meant to be separate. A good example is using multiple global variables (like test in the example below).
+-- - Make sure that the either you switch off let-floating (-fno-full-laziness), or that the call to unsafePerformIO cannot float outside a lambda. For example, if you say: f x = unsafePerformIO (newIORef []) you may get only one reference cell shared between all calls to f. Better would be f x = unsafePerformIO (newIORef [x]) because now it can't float outside the lambda.
+{-# OPTIONS_GHC -fno-cse -fno-full-laziness #-}
+
 -- | Module of adapters for values
 --
 -- Keep this module as small as possible, because it will be imported to adapt
