dc22d410b281ba9ed336bd5ff940ca3838a256d0
Tom Sydney Kerckhove <syd@cs-syd.eu>
2022-12-01 13:42:09 +0100

    e2e test for yesod

--- e2e-test/default.nix
+++ e2e-test/default.nix
@@ -117,26 +117,25 @@ let
       ];
     };
 
-    # It's not clear what is broken here yet.
-    # yesod-report = pkgs.dekking.makeCoverageReport {
-    #   name = "yesod-coverage-report";
-    #   packages = [
-    #     "yesod"
-    #     # "yesod-auth" # Simplified subsumption
-    #     # "yesod-auth-oauth" # Marked as broken
-    #     "yesod-bin" # Expression coverage is broken
-    #     "yesod-core" # Expression coverage is broken
-    #     "yesod-eventsource"
-    #     "yesod-form"
-    #     "yesod-form-multi"
-    #     "yesod-newsfeed"
-    #     "yesod-persistent"
-    #     "yesod-sitemap"
-    #     "yesod-static"
-    #     "yesod-test"
-    #     "yesod-websockets"
-    #   ];
-    # };
+    yesod-report = pkgs.dekking.makeCoverageReport {
+      name = "yesod-coverage-report";
+      packages = [
+        "yesod"
+        # "yesod-auth"
+        # "yesod-auth-oauth" # Marked as broken
+        "yesod-bin"
+        "yesod-core"
+        "yesod-eventsource"
+        "yesod-form"
+        "yesod-form-multi"
+        "yesod-newsfeed"
+        "yesod-persistent"
+        "yesod-sitemap"
+        "yesod-static"
+        "yesod-test"
+        "yesod-websockets"
+      ];
+    };
   };
 in
 (pkgs.linkFarm "e2e-tests" (builtins.attrValues (builtins.mapAttrs
